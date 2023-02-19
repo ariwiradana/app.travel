a7a34cf4d2674471fa29d202d7394e6ea08af1fd
Ari Wiradana <ari@limecommerce.com>
2023-02-19 22:45:49 +0800

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
+import useNProgress from "@/hooks/nprogress/useNProgress";
 import "@/styles/globals.css";
 import "swiper/css";
 import "swiper/css/pagination";
 
 export default function App({ Component, pageProps }) {
+  useNProgress();
   return <Component {...pageProps} />;
 }
