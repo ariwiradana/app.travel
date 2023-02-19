01b365e30475a9a534e9f6623842b326ab99666f
Ari Wiradana <ari@limecommerce.com>
2023-02-19 22:47:10 +0800

    update

--- components/molecules/footer/index.jsx
+++ components/molecules/footer/index.jsx
@@ -6,7 +6,7 @@ import Container from "../container";
 const Footer = ({ menu }) => {
   return (
     <>
-      <Container className="flex px-4 py-8 lg:p-10 bg-slate-50 mt-12 w-full">
+      <Container className="flex px-4 py-8 lg:p-10 bg-gray-50 mt-12 w-full">
         <div className="flex justify-start flex-col items-start">
           <Logo className="transform lg:scale-110" />
           <ul className="flex md:flex-row flex-col md:items-center gap-4 lg:gap-10 mt-8">
@@ -17,7 +17,7 @@ const Footer = ({ menu }) => {
             ))}
           </ul>
         </div>
-        <div className="border-t border-t-slate-200 mt-10 py-6 flex md:flex-row flex-col gap-y-4 md:gap-y-0 justify-between items-center">
+        <div className="border-t border-t-gray-200 mt-10 py-6 flex md:flex-row flex-col gap-y-4 md:gap-y-0 justify-between items-center">
           <p className="font-raleway text-sm text-gray-500 font-medium">
             © 2022 Karens Tour. All rights reserved.
           </p>
